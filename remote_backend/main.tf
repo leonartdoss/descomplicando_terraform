81ec70278e3a443baf0091a0390b7c5bc5252a70
Leonardo Gama <leonardo.lgama@gmail.com>
2025-03-09 23:52:00 +0100

    [day 1][remote_backend]: change ssh key name

--- remote_backend/main.tf
+++ remote_backend/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name = "personal-connection"
+  key_name      = "descomplicando-terraform"
 
   tags = {
     Name = "HelloWorld"
