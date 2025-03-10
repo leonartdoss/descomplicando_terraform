2d25dded8d73b95e9ca3a887c520fd5df5fd8b35
Leonardo Gama <leonardo.lgama@gmail.com>
2025-03-10 01:21:28 +0100

    [day 2][locking state]: Change backend key

--- day_2/locking_state/provider.tf
+++ day_2/locking_state/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket        = "descomplicando-terraform-leonartdoss"
-    key           = "terraform-remote_backend"
+    key           = "terraform_locking_state"
     region        = "us-east-1"
     use_lockfile  = true
   }
