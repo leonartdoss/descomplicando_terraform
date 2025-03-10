1cf3f351b18a65dcc16c9d091b5a55875c64c263
Leonardo Gama <leonardo.lgama@gmail.com>
2025-03-10 01:22:07 +0100

    [day 2][workspaces]: Main files

---
+++ day_2/workspace/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket        = "descomplicando-terraform-leonartdoss"
+    key           = "terraform_workspaces"
+    region        = "us-east-1"
+    use_lockfile  = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
