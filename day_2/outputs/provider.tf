c4d52048bc733e3f2ba9bf06975d22cfc7b15676
Leonardo Gama <leonardo.lgama@gmail.com>
2025-03-12 11:59:16 +0100

    [day 2][outputs]: Use remote backend

--- day_2/outputs/provider.tf
+++ day_2/outputs/provider.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "descomplicando-terraform-leonartdoss"
+    key    = "terraform_outputs"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
